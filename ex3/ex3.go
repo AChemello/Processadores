b5ea9d292af7df150a2d9d78dfecba2876f27156
AChemello <arthur.chemello@edu.pucrs.br>
2026-08-31 22:21:23 +0000

    fix: exec3

--- ex3/ex3.go
+++ ex3/ex3.go
@@ -1,19 +1,27 @@
 package main
+
 import (
-"fmt"
-"sync"
+	"fmt"
+	"sync"
 )
+<<<<<<< HEAD
 var {
  contador int
  mu sync.Mutex
 }
+=======
+
+var contador int
+
+>>>>>>> dace4d7 (fix: exec3)
 func incrementar(n int, wg *sync.WaitGroup) {
-defer wg.Done()
-for i := 0; i < n; i++ {
- contador++
-}
+	defer wg.Done()
+	for i := 0; i < n; i++ {
+		contador++
+	}
 }
 func main() {
+<<<<<<< HEAD
 var wg sync.WaitGroup
  wg.Add(2)
 mu.Lock()
@@ -23,4 +31,12 @@ go incrementar(2000000, &wg)
 mu.Unlock()
  wg.Wait()
  fmt.Printf("Contador: %d (esperado: 4000000)\n", contador)
+=======
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go incrementar(2000000, &wg)
+	go incrementar(2000000, &wg)
+	wg.Wait()
+	fmt.Printf("Contador: %d (esperado: 4000000)\n", contador)
+>>>>>>> dace4d7 (fix: exec3)
 }

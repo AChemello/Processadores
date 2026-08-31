ab2fc76d89968737f4870773075d83f5052d90b6
AChemello <arthur.chemello@edu.pucrs.br>
2026-08-31 18:48:51 -0300

    Implement mutex for contador synchronization
    Added a mutex to synchronize access to the contador variable.

--- ex3/ex3.go
+++ ex3/ex3.go
@@ -3,7 +3,10 @@ import (
 "fmt"
 "sync"
 )
-var contador int
+var {
+ contador int
+ mu sync.Mutex
+}
 func incrementar(n int, wg *sync.WaitGroup) {
 defer wg.Done()
 for i := 0; i < n; i++ {
@@ -13,8 +16,11 @@ for i := 0; i < n; i++ {
 func main() {
 var wg sync.WaitGroup
  wg.Add(2)
+mu.Lock()
 go incrementar(2000000, &wg)
+mu.Lock()
 go incrementar(2000000, &wg)
+mu.Unlock()
  wg.Wait()
  fmt.Printf("Contador: %d (esperado: 4000000)\n", contador)
 }
